3765c0b2b98e21a31ee4ee250049f866bfaf82f7
danusha2345 <danik-guitar@ya.ru>
2025-07-06 17:24:00 +0300

    E5a  мутки

--- src/FNavBit.cpp
+++ src/FNavBit.cpp
@@ -135,27 +135,54 @@ int FNavBit::ComposeEphWords(PGPS_EPHEMERIS Ephemeris, unsigned int EphData[4][7
 {
 	signed int IntValue;
 	unsigned int UintValue;
+	
+	// Initialize all data to zero
+	memset(EphData, 0, sizeof(unsigned int) * 4 * 7);
 
 	// PageType 1
-	EphData[0][0] = (1 << 16) | COMPOSE_BITS(Ephemeris->svid, 10, 6) | COMPOSE_BITS(Ephemeris->iodc, 0, 10);
+	// Note: TOW will be added in GetPageData
+	// Bits layout (after 10-bit shift in GetFrameData):
+	// [0] bits 21-16: Page type (6 bits)
+	// [0] bits 15-0: Reserved for TOW (will be filled in GetPageData)
+	EphData[0][0] = COMPOSE_BITS(1, 16, 6);  // Page type = 1
+	// [1] bits 31-28: Reserved for TOW (4 bits)
+	// [1] bits 27-26: Spare (2 bits) 
+	// [1] bits 25-16: IODnav (10 bits)
+	// [1] bits 15-2: t0c (14 bits)
 	UintValue = Ephemeris->toc / 60;
-	EphData[0][1] = COMPOSE_BITS(UintValue, 18, 14);
-	IntValue = UnscaleInt(Ephemeris->af0, -34);
-	EphData[0][1] |= COMPOSE_BITS(IntValue >> 13, 0, 18);
-	EphData[0][2] = COMPOSE_BITS(IntValue, 19, 13);
-	IntValue = UnscaleInt(Ephemeris->af1, -46);
-	EphData[0][2] |= COMPOSE_BITS(IntValue >> 2, 0, 19);
-	EphData[0][3] = COMPOSE_BITS(IntValue, 30, 2);
+	EphData[0][1] = COMPOSE_BITS(Ephemeris->iodc, 16, 10) | COMPOSE_BITS(UintValue, 2, 14);
+	// [1] bits 1-0: af2 MSB (2 bits)
+	// [2] bits 31-28: af2 remaining (4 bits)
 	IntValue = UnscaleInt(Ephemeris->af2, -59);
-	EphData[0][3] |= COMPOSE_BITS(IntValue, 24, 6);
-	EphData[0][3] |= COMPOSE_BITS(Ephemeris->ura, 16, 8);
-	// <--- 41 bits gap for iono correction --->
+	EphData[0][1] |= COMPOSE_BITS(IntValue >> 4, 0, 2);
+	EphData[0][2] = COMPOSE_BITS(IntValue, 28, 4);
+	// [2] bits 27-7: af1 (21 bits)
+	IntValue = UnscaleInt(Ephemeris->af1, -46);
+	EphData[0][2] |= COMPOSE_BITS(IntValue, 7, 21);
+	// [2] bits 6-0: Spare (7 bits) + [3] bit 31: Spare (1 bit) = 8 bits spare total
+	// [3] bits 30-29: E5a HS (2 bits)
+	// [3] bits 28-0: af0 MSB (29 bits)
+	EphData[0][3] = COMPOSE_BITS(Ephemeris->health & 0x3, 29, 2);  // E5a HS
+	IntValue = UnscaleInt(Ephemeris->af0, -34);
+	EphData[0][3] |= COMPOSE_BITS(IntValue >> 2, 0, 29);
+	// [4] bits 31-30: af0 LSB (2 bits)
+	// [4] bits 29-19: ai0 (11 bits) - iono correction
+	// [4] bits 18-8: ai1 (11 bits) - iono correction  
+	// [4] bits 7-0: ai2 MSB (8 bits) - iono correction
+	EphData[0][4] = COMPOSE_BITS(IntValue, 30, 2);
+	// Iono parameters will be added in GetPageData
+	
+	// [5] bits 31-26: ai2 LSB (6 bits) - iono correction
+	// [5] bits 25-21: Region flags (5 bits) - iono correction
+	// [5] bits 20-11: BGD E5a/E1 (10 bits)
+	// [5] bit 10: E5a DVS (1 bit)
+	// [5] bits 9-5: Spare (5 bits)
+	// [5] bits 4-0: GST-WN MSB (5 bits) - will be added in GetPageData
 	IntValue = UnscaleInt(Ephemeris->tgd, -32);
-	EphData[0][4] = COMPOSE_BITS(IntValue >> 3, 0, 7);
-	EphData[0][5] = COMPOSE_BITS(IntValue, 29, 3);
-	EphData[0][5] |= COMPOSE_BITS(Ephemeris->health >> 4, 27, 2);
-	// <--- 32 bits gap for GST --->
-	EphData[0][6] = COMPOSE_BITS(Ephemeris->health >> 3, 26, 1);
+	EphData[0][5] = COMPOSE_BITS(IntValue, 11, 10) | COMPOSE_BITS((Ephemeris->health >> 2) & 0x1, 10, 1);
+	// [6] bits 31-5: GST-WN remaining + GST-TOW (27 bits) - will be added in GetPageData
+	// [6] bits 4-0: Spare (5 bits)
+	EphData[0][6] = 0;  // GST will be added in GetPageData
 
 	// PageType 2
 	EphData[1][0] = (2 << 16) | COMPOSE_BITS(Ephemeris->iodc, 6, 10);
@@ -219,11 +246,15 @@ int FNavBit::ComposeAlmWords(GPS_ALMANAC Almanac[], unsigned int AlmData[2][7], 
 	signed int IntValue;
 	unsigned int UintValue;
 	int toa = (Almanac[0].valid & 1) ? Almanac[0].toa : (Almanac[1].valid & 1) ? Almanac[1].toa : (Almanac[2].valid & 1) ? Almanac[2].toa : 0;
+	
+	// Initialize all data to zero
+	memset(AlmData, 0, sizeof(unsigned int) * 2 * 7);
 
 	// PageType 5
-	AlmData[0][0] = (5 << 16) | (4 << 12) | COMPOSE_BITS(week, 10, 2) | COMPOSE_BITS((toa / 600), 0, 10);	// IODa=4
+	// Note: this page type doesn't need TOW as it's not in the first 4 page types
+	AlmData[0][0] = COMPOSE_BITS(5, 16, 6) | COMPOSE_BITS(4, 12, 4) | COMPOSE_BITS(week, 10, 2) | COMPOSE_BITS((toa / 600), 0, 10);	// Type=5, IODa=4
 	AlmData[0][1] = COMPOSE_BITS(Almanac[0].svid, 26, 6);	// SVID1 starts here
-	IntValue = UnscaleInt(Almanac[0].sqrtA - SQRT_A0, -9);
+	IntValue = UnscaleInt(Almanac[0].sqrtA - SQRT_A0, -11);
 	AlmData[0][1] |= COMPOSE_BITS(IntValue, 13, 13);
 	UintValue = UnscaleUint(Almanac[0].ecc, -16);
 	AlmData[0][1] |= COMPOSE_BITS(UintValue, 2, 11);
@@ -247,7 +278,7 @@ int FNavBit::ComposeAlmWords(GPS_ALMANAC Almanac[], unsigned int AlmData[2][7], 
 	AlmData[0][5] = COMPOSE_BITS(IntValue, 31, 1);
 	AlmData[0][5] |= COMPOSE_BITS((Almanac[1].valid & 1) ? 0 : 1, 29, 2);
 	AlmData[0][5] = COMPOSE_BITS(Almanac[1].svid, 23, 6);	// SVID2 starts here
-	IntValue = UnscaleInt(Almanac[1].sqrtA - SQRT_A0, -9);
+	IntValue = UnscaleInt(Almanac[1].sqrtA - SQRT_A0, -11);
 	AlmData[0][5] |= COMPOSE_BITS(IntValue, 10, 13);
 	UintValue = UnscaleUint(Almanac[1].ecc, -16);
 	AlmData[0][5] |= COMPOSE_BITS(UintValue >> 1, 0, 10);
@@ -260,7 +291,7 @@ int FNavBit::ComposeAlmWords(GPS_ALMANAC Almanac[], unsigned int AlmData[2][7], 
 	AlmData[0][6] |= COMPOSE_BITS(IntValue >> 12, 0, 4);
 
 	// PageType 6
-	AlmData[1][0] = (6 << 16) | (4 << 12);	// IODa=4
+	AlmData[1][0] = COMPOSE_BITS(6, 16, 6) | COMPOSE_BITS(4, 12, 4);	// Type=6, IODa=4
 	AlmData[1][0] |= COMPOSE_BITS(IntValue, 0, 12);
 	IntValue = UnscaleInt(Almanac[1].omega_dot / PI, -33);
 	AlmData[1][1] = COMPOSE_BITS(IntValue, 21, 11);
@@ -273,7 +304,7 @@ int FNavBit::ComposeAlmWords(GPS_ALMANAC Almanac[], unsigned int AlmData[2][7], 
 	AlmData[1][2] |= COMPOSE_BITS(IntValue, 8, 13);
 	AlmData[1][2] |= COMPOSE_BITS((Almanac[1].valid & 1) ? 0 : 1, 6, 2);
 	AlmData[1][2] |= COMPOSE_BITS(Almanac[2].svid, 0, 6);	// SVID3 starts here
-	IntValue = UnscaleInt(Almanac[2].sqrtA - SQRT_A0, -9);
+	IntValue = UnscaleInt(Almanac[2].sqrtA - SQRT_A0, -11);
 	AlmData[1][3] = COMPOSE_BITS(IntValue, 19, 13);
 	UintValue = UnscaleUint(Almanac[2].ecc, -16);
 	AlmData[1][3] |= COMPOSE_BITS(UintValue, 8, 11);
@@ -304,13 +335,19 @@ void FNavBit::GetPageData(int svid, int page, int subframe, unsigned int GST, un
 	switch (page)
 	{
 	case 0:	// page 1
-		memcpy(Data, GalEphData[svid-1][0], sizeof(unsigned int) * 7);
-		// add iono correction
-		Data[3] |= GalIonoData[0];
-		Data[4] |= GalIonoData[1];
-		// add GST
-		Data[5] |= COMPOSE_BITS(GST >> 5, 0, 27);
-		Data[6] |= COMPOSE_BITS(GST, 27, 5);
+		{
+			unsigned int TOW = GST & 0xFFFFF;  // Extract 20-bit TOW
+			memcpy(Data, GalEphData[svid-1][0], sizeof(unsigned int) * 7);
+			// Add TOW (20 bits)
+			Data[0] |= COMPOSE_BITS(TOW >> 4, 0, 16);  // TOW upper 16 bits
+			Data[1] |= COMPOSE_BITS(TOW, 28, 4);       // TOW lower 4 bits
+			// add iono correction
+			Data[3] |= GalIonoData[0];
+			Data[4] |= GalIonoData[1];
+			// add GST (32 bits)
+			Data[5] |= COMPOSE_BITS(GST >> 27, 0, 5);   // GST upper 5 bits
+			Data[6] |= COMPOSE_BITS(GST, 5, 27);        // GST lower 27 bits
+		}
 		break;
 	case 1:	// page 2
 		memcpy(Data, GalEphData[svid-1][1], sizeof(unsigned int) * 7);
